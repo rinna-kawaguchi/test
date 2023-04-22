563079aeb43aa34e37ce8bd066dcd4b5ed33a54a
UserName <rin3810400h@gmail.com>
2023-04-22 20:36:51 +0900

    9.98秒追加

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,10 @@ public class Main {
         sprinters.stream()
                 .sorted(Comparator.comparing(Sprinter::getAge).reversed())
                 .forEach(sprinter -> System.out.println(sprinter.getName()));
+
+        System.out.println("＜タイムが9.98秒の選手＞");
+        sprinters.stream()
+                .filter(sprinter ->  sprinter.getBestTime() == 9.98)
+                .forEach(sprinter -> System.out.println(sprinter.getName()));
     }
 }
